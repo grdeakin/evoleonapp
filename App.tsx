e24c00eb3bbbef9a0b03d85c234113e34121ebeb
Glenn Richmond <grichmond@crmonline.com.au>
2022-05-08 20:40:24 +0800

    No Ticket: Checking in working map with finding of local position.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,56 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import BurgerMenu from './components/burgermenu/ReactBurgerMenu';
 
-export default function App() {
+const App = () => {
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const [errorMsg, setErrorMsg] = React.useState(null);
+  const [location, setLocation] = React.useState(null);
+  const [region, setRegion] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+  
+      let location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Balanced,
+        timeInterval: 5
+      });
+      console.log(location);
+      // setRegion(location)
+      setmapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,    
+      })
+
+  
+    })();
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <BurgerMenu/>
+      <MapView
+        style={{ alignSelf: 'stretch', height: '80%' }}
+        region={mapRegion}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -22,3 +64,5 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
   },
 });
+
+export default App;
